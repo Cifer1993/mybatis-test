97464a570cf7674982469c3a23dbe33159d67ab8
Lucifer1993 <18811458650@163.com>
2018-10-21 15:10:35 +0800

    SqlsessionFactory初使用

---
+++ mybatis01-test/src/main/java/com/dao/UserMapper.java
@@ -0,0 +1,11 @@
+package com.dao;
+
+import com.pojo.User;
+
+/**
+ * Created by Administrator on 2018/10/14.
+ */
+public interface UserMapper {
+
+    public User select(Integer id);
+}
---
+++ mybatis01-test/src/test/java/Test.java
@@ -0,0 +1,25 @@
+import com.pojo.User;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by Administrator on 2018/10/21.
+ */
+@Slf4j
+public class Test {
+    @org.junit.Test
+    public void test() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        User user = sqlSession.selectOne("com.jiagouedu.mybatis.UserMapper.selectUser", 1);
+
+    }
+}
